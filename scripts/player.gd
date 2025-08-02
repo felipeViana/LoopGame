dcabb32d36db99f3064cb1fa26a4a923b1a8b834
Felipe Viana <felipesousadeveloper@gmail.com>
2025-08-01 23:06:44 -0300

    create basic platform movement

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 300
+var gravity = 30
+var jump_force = 600
+
+var max_speed = 1000
+
+func _physics_process(delta: float) -> void:
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	
+	if !is_on_floor():
+		velocity.y += gravity
+		
+	if velocity.y >= max_speed:
+		velocity.y = max_speed
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jump_force
+	
+	velocity.x = speed * horizontal_direction
+	
+	move_and_slide()
