93283f957aa79338eb1367db8becb8e350145bbd
Vinicius <viniciusguita64@gmail.com>
2025-08-02 09:58:50 -0300

    added animations jump and idle

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 var speed = 300
 var gravity = 30
 var jump_force = 600
-
+@onready var animation_player=$AnimationPlayer
+@onready var sprite=$Sprite2D
 var max_speed = 1000
 
 func _physics_process(delta: float) -> void:
@@ -21,3 +22,12 @@ func _physics_process(delta: float) -> void:
 	velocity.x = speed * horizontal_direction
 	
 	move_and_slide()
+	update_animations()
+	
+	
+func update_animations():
+	if velocity.y<0:
+		animation_player.play("Jump")
+	else:
+		animation_player.play("idle")
+	
